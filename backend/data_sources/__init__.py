7c26b05dbffcc7556168812d44e608efec0a30b8
Yaw Osei <adjeiyawosei@gmail.com>
2026-07-17 18:57:45 +0000

    refactor(sources): drop removed connector exports

--- backend/data_sources/__init__.py
+++ backend/data_sources/__init__.py
@@ -2,7 +2,6 @@ from .base        import DataSource
 from .era5        import ERA5DataSource
 from .open_meteo  import OpenMeteoDataSource
 from .sentinel5p  import Sentinel5PDataSource
-from .modis       import MODISDataSource
 from .worldpop    import WorldPopDataSource
 from .srtm        import SRTMDataSource
 from .orchestrator import DataOrchestrator
@@ -12,7 +11,6 @@ __all__ = [
     "ERA5DataSource",
     "OpenMeteoDataSource",
     "Sentinel5PDataSource",
-    "MODISDataSource",
     "WorldPopDataSource",
     "SRTMDataSource",
     "DataOrchestrator",
